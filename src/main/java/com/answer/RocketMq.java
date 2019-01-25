591d9c806cdd4203abd6ec1cfeb3f0319ba42483
suchao <persistsuchao@hotmail.com>
2019-01-25 19:42:01 +0800

    rocketMQ

--- src/main/java/com/answer/RocketMq.java
+++ src/main/java/com/answer/RocketMq.java
@@ -1,10 +1,18 @@
 package com.answer;
 
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageExt;
 import org.apache.rocketmq.remoting.common.RemotingHelper;
+import org.junit.Test;
+
+import java.util.List;
 
 
 /**
@@ -18,11 +26,27 @@ public class RocketMq {
         producer.setNamesrvAddr("localhost:9876");
         producer.start();
         for (int i = 0; i < 100; i++) {
-            Message msg = new Message("TopicTest" , "TagA" , ("Hello RocketMq"+i).getBytes(RemotingHelper.DEFAULT_CHARSET));
+            Message msg = new Message("TopicTest", "TagA", ("Hello RocketMq" + i).getBytes(RemotingHelper.DEFAULT_CHARSET));
             SendResult sendResult = producer.send(msg);
             System.out.printf("%s%n", sendResult);
         }
         producer.shutdown();
 
+    }
+
+    @Test
+    public void consumer() throws MQClientException {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("rocket_test1_group");
+        consumer.setNamesrvAddr("localhost:9786");
+        consumer.subscribe("TopicTest", "*");
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> list, ConsumeConcurrentlyContext consumeConcurrentlyContext) {
+                System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), list);
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        consumer.start();
+        System.out.printf("Consumer Started.%n");
     }
 }
